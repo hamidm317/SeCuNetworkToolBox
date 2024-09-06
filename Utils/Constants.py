d12b0e50421a98e1013f1c936dc25d435a8f26dd
hamidm317 <hamidm317@gmail.com>
2024-09-07 01:47:34 +0330

    Add Constants and Local to Be more Expandable

---
+++ Utils/Constants.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+class FigConstants:
+
+    StaticGraph = {
+
+        'figsize': (5, 5),
+        'edgesize': 15,
+        'labelsize': 10,
+        'ArrowHeadWidth': 0.05,
+        'ArrowBodyWidth': 0.001,
+        'ArrowHeadLength': 0.1
+
+    }
+
+class LocalDataConstants():
+
+    directories = {
+
+        'eeg_file_dir': r'E:\HWs\Msc\Research\Research\Depression Dataset\New Datasets\Clustered_SingleTrialData_All_Neg_Pos_Stim.mat',
+        'beh_dir_file': r'E:\HWs\Msc\Research\Research\Depression Dataset\depression_rl_eeg\Depression PS Task\Scripts from Manuscript\Data_4_Import.xlsx',
+        'perform_data_dir': r'E:\HWs\Msc\Research\Research\Depression Dataset\New Datasets\Subjects_Behavioral_datas.csv',
+        'eeg_prep_datasets_dir': r'E:\\HWs\Msc\\Research\\Research\\Depression Dataset\\Testing Preprocess',
+        'confile_dir': r'D:\AIRLab_Research\Data\ConnectivityDataDict.pickle',
+        'n_confile_dir': r'D:\AIRLab_Research\Data',
+        'plotSave_dir': r'D:\AIRLab_Research\Plots',
+        'ListOfAvailableSubjects': r'D:\AIRLab_Research\Data\BehavioralData\AvailableSubjects.npy',
+        'fd_excel_dir': r'D:\AIRLab_Research\Features\FeatureDraft.xlsx'
+    }
+
+    names = {
+
+        'JulyClusterNames': ['PF', 'LF', 'RF', 'MFC', 'LT', 'RT', 'LFC', 'RFC', 'MPC', 'LPC', 'RPC', 'MP', 'LPO', 'RPO'],
+        'freq_bands': ['Delta', 'Theta', 'Alpha', 'Beta', 'Gamma', 'LowBeta', 'HighBeta', 'LowGamma', 'MidGamma', 'HighGamma', 'All'],
+        'events': ['All', 'Neg', 'Pos', 'Stim'],
+        'LocalCM':{
+
+            'Transfer Entropy': 'TE',
+            'PLI': 'PLI',
+            'Granger Causality': 'LRB_GC',
+            'dPLI': 'dPLI',
+            'wPLI': 'wPLI',
+            'PLV': 'PLV',
+
+            'Stimulus': 'Stim',
+            'Stim': 'Stim',
+            'PosNeg': 'PosNeg',
+            'Feedback': 'All',
+            'Action': 'Action',
+            'All': 'All'
+
+
+        }
+    }
+
+    NetworksOfInterest = {
+
+        'All': np.arange(14),
+        'ZeroAxis': [0, 3, 8, 11],
+        'Frontal': [0, 1, 2, 3],
+        'OcciTemporal': [4, 5, 12, 13]
+
+    }
+
+    Labels = {
+
+        'groups': ['Control', 'Depressed'],
+        'data_block': ['Block 1', 'Block 2']
+    }
+
+    DefaulValues = {
+
+        'overlap_ratio': 0.98,
+        'window_length': 100,
+        'trial_in_block': 10, # -> In Stim Locked Analyses
+        'min_trial': 20, # -> In PosNeg Locked Analyses
+
+        'Circuit':{
+
+            'ZeroPoint': 25,
+            'Fs': 250
+        },
+
+        'Univariate':{
+
+            'ZeroPoint': 100,
+            'Fs': 500
+        },
+
+    }
---
+++ Utils/Local.py
@@ -0,0 +1,81 @@
+from pandas import read_excel, read_csv
+import numpy as np
+import os
+import pandas as pd
+from datetime import datetime
+
+import pickle
+
+from Utils.Constants import LocalDataConstants
+
+def ExperimentDataLoader():
+
+    BehavioralData = read_excel(LocalDataConstants.directories['beh_dir_file'])
+    Performance_data = read_csv(LocalDataConstants.directories['perform_data_dir'])
+
+    return BehavioralData, Performance_data
+
+def AvailableSubjects():
+
+    SOI = np.load(LocalDataConstants.directories['ListOfAvailableSubjects'])
+
+    return SOI
+
+def HandleDir(Directory):
+
+    if not os.path.isdir(Directory):
+
+        os.makedirs(Directory)
+
+    return Directory
+
+def HandleFileName(SaveFileDir, specs):
+
+    CurrentTime = datetime.now()
+
+    if not os.path.isfile(SaveFileDir + "\\VersionHistory.csv"):
+
+        HistoryDict = {'idx': 0, 'VersionNumber': [0], 'Date': [str(CurrentTime.year) + '-' + str(CurrentTime.month) + '-' + str(CurrentTime.day)], 
+                       'Window_Length': [specs['window_length']], 'Overlap_Ratio': [specs['overlap_ratio']], 'Start Time': [specs['start time']],
+                       'End Time': [specs['end time']],
+                       'OrdersMatrix': [specs['orders_matrix']]}
+        DF = pd.DataFrame(HistoryDict)
+
+        DF.to_csv(SaveFileDir + "\\VersionHistory.csv", index = False)
+
+        version_number = 0
+
+    else:
+
+        HistoryDict = read_csv(SaveFileDir + "\\VersionHistory.csv", index_col = 0)
+
+        version_number = HistoryDict['VersionNumber'][len(HistoryDict['VersionNumber']) - 1] + 1
+
+        new_row = [version_number, str(CurrentTime.year) + '-' + str(CurrentTime.month) + '-' + str(CurrentTime.day), specs['window_length'],
+                   specs['overlap_ratio'], specs['start time'], specs['end time'], specs['orders_matrix']]
+        
+        HistoryDict.loc[len(HistoryDict['VersionNumber'])] = new_row
+        HistoryDict.sort_index()
+
+        HistoryDict.to_csv(SaveFileDir + "\\VersionHistory.csv")
+
+    SaveFileName = "Data_Version" + str(version_number)
+
+    return SaveFileName, version_number
+    
+def HandleDataLoad(Dir, version_number = None):
+
+    assert os.path.isdir(Dir), "This Data is not Available"
+
+    if version_number is None:
+
+        VersionHistoryDF = read_csv(Dir + "\\VersionHistory.csv", index_col = 0)
+        version_number = np.array(VersionHistoryDF['VersionNumber'])[-1]
+
+    LoadFileDir = Dir + "\\Data_Version" + str(version_number)
+
+    with open(LoadFileDir, 'rb') as f:
+
+        ConDataDict = pickle.load(f)
+
+    return ConDataDict
